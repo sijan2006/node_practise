26870febcae8671971681a5385afde3484b81752
sijan2006 <sijansap11@gmail.com>
2026-05-27 16:50:41 +0545

    lesson-4: objects - create, update, delete, loop, array of objects

--- les3-objects/objects.js
+++ les3-objects/objects.js
@@ -12,18 +12,27 @@ const person = {
 };
 
 // --- TODO 1: Print the full object ---
+console.log(person);
 
 // --- TODO 2: Access and print just the name and age ---
+console.log(`The name is ${person.name} and age is ${person.age} `);
 
 // --- TODO 3: Add a new property 'country' with your country name ---
 // Print the updated object
+person.country="Nepal";
+console.log(person);
 
 // --- TODO 4: Update the age to 21 ---
+person.age=21;
 // Print the updated object
+console.log(person);
 
 // --- TODO 5: Delete the 'isStudent' property ---
 // Hint: delete person.isStudent
+
+delete person.isStudent;
 // Print the updated object
+console.log(person);
 
 // --- TODO 6: Loop through ALL properties using for...in ---
 // Print each key and value like:
@@ -31,6 +40,22 @@ const person = {
 // age : 21
 // hobby : Sleeping
 
+
+
+for (let per in person){
+    console.log(` ${per} : ${person[per]} `);
+}
+
 // --- TODO 7: Create an ARRAY of 3 person objects ---
 // Each person has: name, age, country
+
+let arr = [
+    { name: "Rijan", age: 23, country: "Nepal" },
+    { name: "Vijan", age: 29, country: "USA" },
+    { name: "Pigeon", age: 3, country: "Italy" }
+];
+
 // Loop through and print each person's name and country
+arr.forEach((ar)=>{
+    console.log(`The name is ${ar.name} and country is ${ar.country} `);
+});
